8216b37de2fc0424702fd119104c410dcae45f33
Noel-Anthony-W <weissanthony.code@gmail.com>
2025-06-08 12:27:49 +0200

    home page wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,17 @@
         <div id="cta" class="hero">
             <div class="hero-cta">
                 <div>
-                    <h1 class="h1">Torten die für</h1>
-                    <h1 class="h1 offset-2rem">Magische Momente Sorgen</h1>
+                    <h1 class="h1">Torten die für Magische Momente Sorgen</h1>
                 </div>
                 <h2>Hier finden Sie die besten Torten für jeden Anlass!</h2>
 
                 <div class="hero-buttons">
-                    <x-button href="{{ route('order') }}" >Jetzt Bestellen</x-button>
-                    <x-button href="{{ route('order') }}" :primary="false">Kuchen entdecken</x-button>                    
+                    <x-button href="{{ route('order') }}" class="h5-font-size" >Jetzt Bestellen</x-button>
+                    <x-button href="{{ route('order') }}" class="h5-font-size" :primary="false">Kuchen entdecken</x-button>                    
                 </div>
             </div>
             <div class="hero-image">
-                <img src="{{ asset('images/hero.jpg') }}" alt="Hero Image" class="hero-img">
+                <img src="{{ asset('images/hero.svg') }}" alt="Hero Image" class="hero-img">
         </div>
     </section>
 <section>    
